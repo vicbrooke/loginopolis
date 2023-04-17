1dc3124ff4b11772e464459a5ed8bfc7261085a7
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2023-04-17 13:49:32 +0100

    register and login endpoints

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { User } = require('./db');
+const { User } = require("./db");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', async (req, res, next) => {
+app.get("/", async (req, res, next) => {
   try {
-    res.send('<h1>Welcome to Loginopolis!</h1><p>Log in via POST /login or register via POST /register</p>');
+    res.send(
+      "<h1>Welcome to Loginopolis!</h1><p>Log in via POST /login or register via POST /register</p>"
+    );
   } catch (error) {
     console.error(error);
-    next(error)
+    next(error);
   }
 });
 
 // POST /register
 // TODO - takes req.body of {username, password} and creates a new user with the hashed password
 
+app.post("/register", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const hashPw = await bcrypt.hash(password, 5);
+    const newUser = await User.create({ username, password: hashPw });
+    res.status(200).send(`successfully created user ${newUser.username}`);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
 // POST /login
 // TODO - takes req.body of {username, password}, finds user by username, and compares the password with the hashed version from the DB
+app.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ where: { username } });
+    const result = await bcrypt.compare(password, user.password);
+    if (!result) {
+      res.status(401).send(`incorrect username or password`);
+    } else {
+      res.status(200).send(`successfully logged in user ${user.username}`);
+    }
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
 
 // we export the app, not listening in here, so that we can run tests
 module.exports = app;
