45da72551e6bbd9a67a56f22785ed64558fd57aa
vicbrooke <113979837+vicbrooke@users.noreply.github.com>
2023-04-17 14:13:09 +0100

    Hashed passwords for seeded users

--- db/seedFn.js
+++ db/seedFn.js
@@ -1,10 +1,19 @@
-const {sequelize} = require('./db');
-const {User} = require('./');
-const users = require('./seedData');
+const { sequelize } = require("./db");
+const { User } = require("./");
+const users = require("./seedData");
+const bcrypt = require("bcrypt");
 
 const seed = async () => {
   await sequelize.sync({ force: true }); // recreate db
-  await User.bulkCreate(users);
+
+  const hashedUsers = await Promise.all(
+    users.map(async (user) => {
+      user.password = await bcrypt.hash(user.password, 5);
+      return user;
+    })
+  );
+
+  await User.bulkCreate(hashedUsers);
 };
 
 module.exports = seed;
